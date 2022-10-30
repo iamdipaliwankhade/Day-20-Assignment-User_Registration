b1c12e3886f2a2e4dcfdce8f473f7b2c8a7b901e
iamdipaliwankhade <iamdipaliwankhade@gmail.com>
2022-10-30 17:20:33 +0530

    [Dipali] Added : first no of validation

--- src/com/userregistration/UserRegistration.java
+++ src/com/userregistration/UserRegistration.java
@@ -1,8 +1,16 @@
 package com.userregistration;
 
-public class UserRegistration {
-	public static void main(String[] args) {
-		System.out.println("Welcome to User_Registration Program");
-	}
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import org.junit.Test;
+
+public class UserRegistration {
+
+	public boolean firstName(String firstName) {
+		String regex = "^[A-Z]{1}[a-z]{2,}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(firstName);
+		return matcher.matches();
+	}
 }
---
+++ src/com/userregistration/UserRegistrationTest.java
@@ -0,0 +1,21 @@
+package com.userregistration;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class UserRegistrationTest {
+	UserRegistration userRegistration = new UserRegistration();
+
+	@Test
+	public void givenFirstName_WhenProper_ShouldReturnTrue() {
+		boolean result = userRegistration.firstName("Dipali");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenFirstName_WhenNotProper_ShouldReturnFalse() {
+		boolean result = userRegistration.firstName("dipali");
+		Assert.assertEquals(false, result);
+	}
+}
